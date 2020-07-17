2454e2e6e2e6d55f156eb51b34ecc59baecb2d41
Garrett Summerfield <garrettsummerfi3ld@gmail.com>
2020-07-17 00:36:45 -0500

    Error checking, looks for files, and some other fixes
    - vars.json is where most environment variables are stored, keeps source, destination paths and specific files types you want to move over.
    - Created a requirements.txt
    - Changed up README.md
    - Error handling is now a lot better than before
    - Added more documentation
    - Manifested checkFiles()

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-copyIngest
+# copyIngest
+
+***Work In Progress***
+
+Copies files from a source location to a destination location, preserving file structure and formatting it by date.
+
+## How to use
+
+1. Clone repo `git clone https://github.com/garrettsummerfi3ld/copyIngest.git`
+2. Enter repo `cd copyIngest`
+3. Modify `vars.json` to your personal liking, with source and destination paths, and even file types
+4. `pip -m install requirements.txt`
+5. `python copyIngest.py`
--- copyIngest.py
+++ copyIngest.py
@@ -1,79 +1,112 @@
-import os.path as path
 import shutil
 import os
 import datetime
+import sys
+import json
+import os.path as path
 from colorama import init, Fore, Back, Style
 from termcolor import colored
 
 ### VARS
-sourcePath = "G:\\Ingest"
-destPath = "E:\\Videos\\Ingest"
-currentDateTime = datetime.datetime
+with open("vars.json", "r") as vars:
+    data = json.load(vars)
+sourcePath = data["sourcePath"]
+destPath = data["destPath"]
+fileTypes = data["fileTypes"]
+currentDateTime = datetime.datetime.now()
+sourceFiles = []
+
 
 ### COLOR LOGGING
-init()
+init(autoreset=True)
 
-# Green
+# Success -> green
 def success(args): print(Fore.GREEN + args)
 
-# Normal
-def info(args): print(Style.RESET_ALL + args)
+# Normal -> white
+def info(args): print(args)
 
-# Yellow
+# Warning -> yellow
 def warn(args): print(Fore.YELLOW + args)
 
-# Red
+# Error -> red
 def err(args): print(Fore.RED + args)
 
-# Cyan
+# Critical -> red bg
+def crit(args): print(Back.RED + Fore.WHITE + args)
+
+# Debug -> Cyan
 def debug(args): print(Fore.CYAN + args)
 
 ### FUNCTIONS
 
 # Checks location of sourcePath if it is a real location
 def sourcePathCheck():
-    debug("Started sourcePathCheck()")
     info("Checking source path...")
-    if (path.exists(sourcePath) == False):
-        debug("Path exists bool =" + str(path.exists(sourcePath)))
-        warn("Source location does not exist!")
-        info("Creating source location...")
-        os.mkdir(sourcePath)
-        success("Created source location!")
-        pass
+    if (sourcePath != ""):
+        if (path.exists(sourcePath) == False):
+            warn("Source location does not exist!")
+            info("Creating source location...")
+            os.mkdir(sourcePath)
+            success("Created source location!")
+            pass
+        else:
+            info("Source location exists!")
+            pass
     else:
-        debug("Path exists bool =" + str(path.exists(sourcePath)))
-        info("Source location exists!")
-        pass
+        raise FileNotFoundError("'sourcePath' in 'vars.json' is left blank. Edit the file to fix this issue")
+
 
 # Checks location of destPath if it is a real location
 def destPathCheck():
     info("Checking destination path...")
-    if (path.exists(destPath) == False):
-        warn("Destination location does not exist!")
-        info("Creating destination location")
-        os.mkdir(destPath)
-        success("Created destination location")
-        pass
+    if (destPath != ""):
+        if (path.exists(destPath) == False):
+            warn("Destination location does not exist!")
+            info("Creating destination location")
+            os.mkdir(destPath)
+            success("Created destination location")
+            pass
+        else:
+            info("Destination location exists!")
+            pass
     else:
-        info("Destination location exists!")
+        raise FileNotFoundError("'destPath' in 'vars.json' is left blank. Edit the file to fix this issue")
 
-# File checking
+# Checks for files in the sourcePath
 def checkFiles():
-    debug
+    info("Checking files in sourcePath...")
+    for r, d, f in os.walk(data["sourcePath"]):
+        for file in f:
+            if data["fileTypes"] in file:
+                sourceFiles.append(path.join(r, file))
+
+    for f in sourceFiles:
+        info("Found {}".format(f))
+
+    if len(sourceFiles) == 0:
+        err("There are no files! Exiting as there is nothing to do...")
+        SystemExit(1)
 
 # Main function
 def main():
-    debug("Started main()")
+    info("Started at {}".format(str(currentDateTime)))
+
     try:
+        debug("Checking 'vars.json'...")
+        debug(str(data))
         sourcePathCheck()
         destPathCheck()
+        checkFiles()
+
     except Exception as ex:
-        err("Something really bad just happened. Printing stacktrace...")
-        err(ex)
+        crit("Something really bad just happened. Printing stacktrace...")
+        print(ex)
+        print(sys.exc_info())
+        vars.close()
         SystemExit(1)
 
 # Init function
 if __name__ == "__main__":
-    debug("Starting up...")
+    info("Starting up...")
     main()
